00cdaa90bdc1ac90575ee8656880ea1da7b96047
Jonika <katyadyduch@gmail.com>
2025-02-16 16:50:25 +0200

    fix unit

--- tests/Unit/Controller/ClassroomControllerTest.php
+++ tests/Unit/Controller/ClassroomControllerTest.php
@@ -7,17 +7,20 @@ use App\Entity\Classroom;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ClassroomControllerTest extends TestCase
 {
     public function testGetClassroomInfo(): void
     {
+        $serializer = $this->createMock(SerializerInterface::class);
         $classroom = $this->createMock(Classroom::class);
-        $controller = new ClassroomController();
+        $controller = new ClassroomController($serializer);
 
         $response = $controller->getClassroomInfo($classroom);
 
         $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
     }
+
 }
